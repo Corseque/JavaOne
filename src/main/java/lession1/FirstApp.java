7028654b420398c5bdf7728cee6d1219a7b23d5b
Corseque <corseque@ya.ru>
2022-02-09 22:35:19 +0300

    Lesson 1 homework

---
+++ src/main/java/lession1/FirstApp.java
@@ -0,0 +1,46 @@
+package lession1;
+
+public class FirstApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign(1 , -2);
+        checkSumSign(1 , 2);
+        printColor(-1);
+        printColor(1);
+        printColor(101);
+        compareNumbers(1 , 2);
+        compareNumbers(2 , 1);
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign(int a, int b) {
+        if ((a + b) > 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor(int value) {
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(int a, int b) {
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
