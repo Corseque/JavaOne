db9ff697363cb63528d5ba379d709ce656cedfc0
Corseque <corseque@ya.ru>
2022-03-18 23:29:24 +0300

    Lesson 8 homework

--- src/main/java/lesson8/FrameApp.java
+++ src/main/java/lesson8/FrameApp.java
@@ -5,38 +5,26 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
-import java.util.Scanner;
 
 public class FrameApp extends JFrame {
-    public static char[][] map;
+    private final String TITLE_confirm = "Окно подтверждения";
     public static final int SIZE = 3;
     public static final int DOTS_TO_WIN = 3;
     public static final char DOT_EMPTY = ' ';
     public static final char DOT_X = 'X';
     public static final char DOT_O = 'O';
-    public static final Scanner SCANNER = new Scanner(System.in);
-    public static final Random RANDOM = new Random();
 
     public FrameApp() {
         setBounds(300, 300, 100 * SIZE, 100 * SIZE);
         setTitle("Tic Tac Toe");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(new GridLayout(SIZE, SIZE));
-//        JButton[] buttons = new JButton[SIZE * SIZE];
-//        for (int i = 0; i < buttons.length; i++) {
-//            buttons[i] = new JButton(String.valueOf(DOT_EMPTY));
-//            add(buttons[i]);
-//        }
-
         JButton[][] buttons = new JButton[SIZE][SIZE];
         for (int i = 0; i < buttons.length; i++) {
             for (int j = 0; j < buttons[i].length; j++) {
                 buttons[i][j] = new JButton(String.valueOf(DOT_EMPTY));
-                //buttons[i][j].setText(String.valueOf(i + 1));
                 add(buttons[i][j]);
-
             }
-
         }
 
         final int[] countFreeCell = {SIZE*SIZE};
@@ -50,21 +38,14 @@ public class FrameApp extends JFrame {
                         if (isCellValid(buttons,finalI,finalJ)) {
                             buttons[finalI][finalJ].setText(String.valueOf(DOT_X));
                             countFreeCell[0]--;
-
                             //проверка на выигрыш человека
                             if (checkWin(DOT_X, buttons)) {
-                                JOptionPane.showMessageDialog(FrameApp.this,"Вы выиграли!");
-                            } else {
-//                                Рандом ход AI
-//                                int i;
-//                                int j;
-//                                do {
-//                                    i = new Random().nextInt(buttons.length);
-//                                    j = new Random().nextInt(buttons[finalI].length);
-//                                } while (!isCellValid(buttons[i][j]) && countFreeCell[0] != 0);
-//                                buttons[i][j].setText(String.valueOf(DOT_O));
-//                                countFreeCell[0]--;
-
+                                int input = JOptionPane.showConfirmDialog(FrameApp.this,"Вы выиграли! Играем ещё?",TITLE_confirm,JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
+                                choiceOnJOptionPane(input, buttons);
+                            } else if (isMapFull(buttons)) {
+                                    int input = JOptionPane.showConfirmDialog(FrameApp.this,"Ничья! Играем ещё?",TITLE_confirm,JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
+                                    choiceOnJOptionPane(input, buttons);
+                                }
                                 //проверка на выигрыш AI или человека
                                 int[] dot;
                                 do {
@@ -78,81 +59,61 @@ public class FrameApp extends JFrame {
                                         } else {
                                             dot[0] = new Random().nextInt(buttons.length);
                                             dot[1] = new Random().nextInt(buttons[finalI].length);
+                                            if (isCellValid(buttons,dot[0],dot[1])) {
+                                                buttons[dot[0]][dot[1]].setText(String.valueOf(DOT_O));
+                                            }
                                         }
                                     }
-                                } while (!isCellValid(buttons,dot[0],dot[1]) && countFreeCell[0] != 0);
-                                buttons[dot[0]][dot[1]].setText(String.valueOf(DOT_O));
+                                } while (buttons[dot[0]][dot[1]].equals(Character.toString(DOT_O))/*!isCellValid(buttons,dot[0],dot[1])*/ /*&& countFreeCell[0] != 0*/);
                                 countFreeCell[0]--;
+                                if (checkWin(DOT_O, buttons)) {
+                                    int input = JOptionPane.showConfirmDialog(FrameApp.this, "Выиграл компьютер! Играем ещё?",TITLE_confirm,JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
+                                    choiceOnJOptionPane(input, buttons);
+                                } else if (isMapFull(buttons)) {
+                                    int input = JOptionPane.showConfirmDialog(FrameApp.this,"Ничья! Играем ещё?",TITLE_confirm,JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
+                                    choiceOnJOptionPane(input, buttons);
+                                }
                             }
                         }
-                    }
                 });
             }
         }
-
-//        for (JButton button : buttons) {
-//            button.addActionListener(new ActionListener() {
-//                @Override
-//                public void actionPerformed(ActionEvent e) {
-//                    if (isCellValid(button)) {
-//                        button.setText(String.valueOf(DOT_X));
-//                        countFreeCell[0]--;
-//
-//
-//                        for (int i = 0; i < buttons.length; i++) {
-//                            System.out.print(buttons[i].getText());
-//                        }
-//                        System.out.println();
-//
-//                        //проверка на выигрыш человека
-//                        if (checkWin(DOT_X, buttons)) {
-//                            JOptionPane.showMessageDialog(FrameApp.this,"Вы выиграли!");
-//                        } else {
-//                            //Рандом ход AI
-////                            int i;
-////                            do {
-////                                i = new Random().nextInt(buttons.length);
-////                            } while (!isCellValid(buttons[i]) && countFreeCell[0] != 0);
-////                            buttons[i].setText(String.valueOf(DOT_O));
-////                            countFreeCell[0]--;
-//
-//                            //проверка на выигрыш AI или человека
-//                            int dot = -1;
-//                            do {
-//                                dot = winDot(DOT_O, buttons);
-//                                if (isCellValid(buttons[dot])) {
-//                                    buttons[dot].setText(String.valueOf(DOT_O));
-//                                } else {
-//                                    dot = winDot(DOT_X, buttons);
-//                                    if (isCellValid(buttons[dot])) {
-//                                        buttons[dot].setText(String.valueOf(DOT_O));
-//                                    } else {
-//                                        int j;
-//                                        do {
-//                                            j = new Random().nextInt(buttons.length);
-//                                        } while (!isCellValid(buttons[j]) && countFreeCell[0] != 0);
-//                                        buttons[j].setText(String.valueOf(DOT_O));
-//                                        countFreeCell[0]--;
-//                                    }
-//                                }
-//                            } while (!isCellValid(buttons[dot]));
-//                        }
-//                    }
-///*                    for (int i = 0; i < buttons.length; i++) {
-//                        System.out.print(buttons[i].getText());
-//                    }
-//                    System.out.println();*/
-//                }
-//            });
-//        }
         setVisible(true);
     }
 
-
-
-
     public static void main(String[] args) {
         new FrameApp();
+    }
+
+    private void choiceOnJOptionPane(int choiceOnJOptionPane, JButton[][] buttons) {
+        switch (choiceOnJOptionPane) {
+            case 0:
+                for (int i = 0; i < buttons.length; i++) {
+                    for (int j = 0; j < buttons[i].length; j++) {
+                        buttons[i][j].setText(String.valueOf(DOT_EMPTY));
+                    }
+                }
+                break;
+
+            case 1: FrameApp.this.dispose();
+                break;
+        }
+    }
+
+    /**
+     * Проверка заполненности поля
+     *
+     * @return
+     */
+    private static boolean isMapFull(JButton[][] buttons) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (buttons[i][j].getText().equals(Character.toString(DOT_EMPTY))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     /**
@@ -161,15 +122,20 @@ public class FrameApp extends JFrame {
      *
      * @return
      */
-    public static boolean isCellValid(JButton[][] buttons, int x, int y) {
+    private static boolean isCellValid(JButton[][] buttons, int x, int y) {
         if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
             return false;
         }
         return buttons[x][y].getText().equals(Character.toString(DOT_EMPTY));
     }
 
-
-    public static boolean checkWin(char sym, JButton[][] buttons) {
+    /**
+     * Проверка выигрыша
+     *
+     * @param sym символ для проверки победы
+     * @return
+     */
+    private static boolean checkWin(char sym, JButton[][] buttons) {
 
         int vertical = 0;
         int horizontal = 0;
@@ -201,31 +167,13 @@ public class FrameApp extends JFrame {
         return (diagonalLeftToRight == DOTS_TO_WIN || diagonalRightToLeft == DOTS_TO_WIN || vertical == DOTS_TO_WIN || horizontal == DOTS_TO_WIN);
     }
 
-
-//        for (int i = 0; i < SIZE - DOTS_TO_WIN + 1; i++) {
-//            for (int j = 0; j < DOTS_TO_WIN; j++) {
-//                if (diagonalLeftToRight == DOTS_TO_WIN || diagonalRightToLeft == DOTS_TO_WIN || vertical == DOTS_TO_WIN || horizontal == DOTS_TO_WIN) {
-//                    break;
-//                } else {
-//                    diagonalLeftToRight = (buttons[j * (SIZE + 1)].getText().equals(Character.toString(sym)) && diagonalLeftToRight < DOTS_TO_WIN) ? ++diagonalLeftToRight : --diagonalLeftToRight;
-//                    diagonalRightToLeft = (buttons[(j + SIZE - 1) * (SIZE - 1)].getText().equals(Character.toString(sym)) && diagonalRightToLeft < DOTS_TO_WIN) ? ++diagonalRightToLeft : --diagonalRightToLeft;
-//
-//                    for (int k = 0; k < DOTS_TO_WIN; k++) {
-//                        horizontal = (buttons[k + j * DOTS_TO_WIN].getText().equals(Character.toString(sym)) && horizontal < DOTS_TO_WIN) ? ++horizontal : --horizontal;
-//                        vertical = (buttons[j + k * DOTS_TO_WIN].getText().equals(Character.toString(sym)) && vertical < DOTS_TO_WIN) ? ++vertical : --vertical;
-//                    }
-//                    vertical = (vertical < DOTS_TO_WIN) ? 0 : vertical;
-//                    horizontal = (horizontal < DOTS_TO_WIN) ? 0 : horizontal;
-//                }
-//            }
-//            diagonalLeftToRight = (diagonalLeftToRight < DOTS_TO_WIN) ? 0 : diagonalLeftToRight;
-//            diagonalRightToLeft = (diagonalRightToLeft < DOTS_TO_WIN) ? 0 : diagonalRightToLeft;
-//        }
-//        return(diagonalLeftToRight == DOTS_TO_WIN || diagonalRightToLeft == DOTS_TO_WIN || vertical == DOTS_TO_WIN || horizontal == DOTS_TO_WIN);
-//}
-
-
-        public static int[] winDot(char sym, JButton[][] buttons) {
+    /**
+     * Проверка возможного выигрыша на следующем шагу
+     * @param sym
+     * @param buttons
+     * @return
+     */
+    private static int[] winDot(char sym, JButton[][] buttons) {
             int vertical = 0;
             int horizontal = 0;
             int diagonalLeftToRight = 0;
@@ -238,7 +186,7 @@ public class FrameApp extends JFrame {
                         if (diagonalLeftToRight == DOTS_TO_WIN - 1 || diagonalRightToLeft == DOTS_TO_WIN - 1 || vertical == DOTS_TO_WIN - 1 || horizontal == DOTS_TO_WIN - 1) {
                             break;
                         } else {
-                            diagonalLeftToRight = (buttons[k + i][k + j].equals(Character.toString(sym)) && diagonalLeftToRight < DOTS_TO_WIN - 1) ? ++diagonalLeftToRight : (diagonalLeftToRight == 0) ? diagonalLeftToRight : --diagonalLeftToRight;
+                            diagonalLeftToRight = (buttons[k + i][k + j].getText().equals(Character.toString(sym)) && diagonalLeftToRight < DOTS_TO_WIN - 1) ? ++diagonalLeftToRight : (diagonalLeftToRight == 0) ? diagonalLeftToRight : --diagonalLeftToRight;
 
                             if (diagonalLeftToRight == DOTS_TO_WIN - 1 && isCellValid(buttons,k + i + 1,k + j + 1)) {
                                 dot[0] = k + i + 1;
@@ -250,7 +198,7 @@ public class FrameApp extends JFrame {
                                 break;
                             }
 
-                            diagonalRightToLeft = (buttons[DOTS_TO_WIN - 1 - k - 1 + i][k + j].equals(Character.toString(sym)) && diagonalRightToLeft < DOTS_TO_WIN - 1) ? ++diagonalRightToLeft : (diagonalRightToLeft == 0) ? diagonalRightToLeft : --diagonalRightToLeft;
+                            diagonalRightToLeft = (buttons[DOTS_TO_WIN - 1 - k - 1 + i][k + j].getText().equals(Character.toString(sym)) && diagonalRightToLeft < DOTS_TO_WIN - 1) ? ++diagonalRightToLeft : (diagonalRightToLeft == 0) ? diagonalRightToLeft : --diagonalRightToLeft;
 
                             if (diagonalRightToLeft == DOTS_TO_WIN - 1 && isCellValid(buttons,DOTS_TO_WIN - 1 - k - 1 + i - 1,k + j + 1)) {
                                 dot[0] = DOTS_TO_WIN - 1 - k - 1 + i - 1;
@@ -262,7 +210,7 @@ public class FrameApp extends JFrame {
                                 break;
                             }
                             for (int l = 0; l < DOTS_TO_WIN - 1; l++) {
-                                horizontal = (buttons[k][l].equals(Character.toString(sym)) && horizontal < DOTS_TO_WIN - 1) ? ++horizontal : (horizontal == 0) ? horizontal : --horizontal;
+                                horizontal = (buttons[k][l].getText().equals(Character.toString(sym)) && horizontal < DOTS_TO_WIN - 1) ? ++horizontal : (horizontal == 0) ? horizontal : --horizontal;
 
                                 if (isCellValid(buttons,k,l + 1)&& horizontal == DOTS_TO_WIN - 1) {
                                     dot[0] = k;
@@ -274,7 +222,7 @@ public class FrameApp extends JFrame {
                                     break;
                                 }
 
-                                vertical = (buttons[l][k].equals(Character.toString(sym)) && vertical < DOTS_TO_WIN - 1) ? ++vertical : (vertical == 0) ? vertical : --vertical;
+                                vertical = (buttons[l][k].getText().equals(Character.toString(sym)) && vertical < DOTS_TO_WIN - 1) ? ++vertical : (vertical == 0) ? vertical : --vertical;
 
                                 if (isCellValid(buttons,l + 1,k) && vertical == DOTS_TO_WIN - 1) {
                                     dot[0] = l + 1;
@@ -296,9 +244,4 @@ public class FrameApp extends JFrame {
             }
             return dot;
         }
-
-
-
-
-
 }
